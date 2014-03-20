3ff459c15c48921885de9808afd82fb144369877
filip <filip@localhost.localdomain>
2014-03-20 13:27:30 +0100

    fixing scripts

--- scripts/flata-termination.sh
+++ scripts/flata-termination.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 #export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib/jni
-export CLASSPATH=./lib/flata.jar:./lib/nts.jar:./lib/antlr-3.3-complete.jar:./lib/glpk-java-4.47.jar
-java verimag.main.Main -term -t-merge-prec -t-fullincl $1
+export CLASSPATH=./flata.jar:./lib/nts.jar:./lib/antlr-3.3-complete.jar:./lib/glpk-java-4.47.jar
+java verimag.flata.Main -term -t-merge-prec -t-fullincl $1
 
